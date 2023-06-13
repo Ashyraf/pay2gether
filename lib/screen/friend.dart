0a5ed5581dc79dfb7112eb12e51fb41ddf487f64
Ashyraf <zmohdashyraf@gmail.com>
2023-06-13 20:57:51 +0800

    friend update done

--- lib/screen/friend.dart
+++ lib/screen/friend.dart
@@ -86,6 +86,78 @@ class _FriendState extends State<Friend> {
     }
   }
 
+  Future<void> acceptFriendRequest(
+      String friendUsername, String friendEmail) async {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    final currentUserEmail = currentUser?.email;
+
+    try {
+      final currentUserRef =
+          FirebaseFirestore.instance.collection('users').doc(currentUserEmail);
+      final currentUserDoc = await currentUserRef.get();
+
+      if (currentUserDoc.exists) {
+        final friendRequests =
+            currentUserDoc['friendRequests'] as List<dynamic>;
+
+        final updatedFriendRequests = friendRequests
+            .where((request) => request['senderUsername'] != friendUsername)
+            .toList();
+
+        await currentUserRef.update({
+          'friendRequests': updatedFriendRequests,
+          'friends': FieldValue.arrayUnion([friendEmail])
+        });
+
+        final friendRef =
+            FirebaseFirestore.instance.collection('users').doc(friendEmail);
+        final friendDoc = await friendRef.get();
+
+        if (friendDoc.exists) {
+          final friendFriendRequests =
+              friendDoc['friendRequests'] as List<dynamic>;
+
+          final updatedFriendFriendRequests = friendFriendRequests
+              .where((request) => request['senderUsername'] != currentUserEmail)
+              .toList();
+
+          await friendRef.update({
+            'friendRequests': updatedFriendFriendRequests,
+            'friends': FieldValue.arrayUnion([currentUserEmail])
+          });
+        }
+      }
+    } catch (error) {
+      print('Error accepting friend request: $error');
+    }
+  }
+
+  Future<void> rejectFriendRequest(String friendUsername) async {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    final currentUserEmail = currentUser?.email;
+
+    try {
+      final currentUserRef =
+          FirebaseFirestore.instance.collection('users').doc(currentUserEmail);
+      final currentUserDoc = await currentUserRef.get();
+
+      if (currentUserDoc.exists) {
+        final friendRequests =
+            currentUserDoc['friendRequests'] as List<dynamic>;
+
+        final updatedFriendRequests = friendRequests
+            .where((request) => request['senderUsername'] != friendUsername)
+            .toList();
+
+        await currentUserRef.update({
+          'friendRequests': updatedFriendRequests,
+        });
+      }
+    } catch (error) {
+      print('Error rejecting friend request: $error');
+    }
+  }
+
   Future<void> getFriendList() async {
     final currentUser = FirebaseAuth.instance.currentUser;
     final currentUserEmail = currentUser?.email;
@@ -165,7 +237,9 @@ class _FriendState extends State<Friend> {
                 usernames: _usernames,
                 onUsernameChanged: searchUsername,
                 onFriendRequest: requestFriendship,
-                currentUserID: currentUserID.isNotEmpty ? currentUserID : '',
+                onAcceptRequest: acceptFriendRequest,
+                onRejectRequest: rejectFriendRequest,
+                currentUserID: currentUserID,
               ),
             ],
           ),
@@ -210,6 +284,8 @@ class MyDrawerList extends StatelessWidget {
   final List<String> usernames;
   final Function(String) onUsernameChanged;
   final Function(String) onFriendRequest;
+  final Function(String, String) onAcceptRequest;
+  final Function(String) onRejectRequest;
   final String currentUserID;
 
   MyDrawerList({
@@ -217,6 +293,8 @@ class MyDrawerList extends StatelessWidget {
     required this.usernames,
     required this.onUsernameChanged,
     required this.onFriendRequest,
+    required this.onAcceptRequest,
+    required this.onRejectRequest,
     required this.currentUserID,
   });
 
@@ -258,6 +336,153 @@ class MyDrawerList extends StatelessWidget {
               );
             },
           ),
+          SizedBox(height: 20),
+          Text(
+            'Friend Requests',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          StreamBuilder<DocumentSnapshot>(
+            stream: FirebaseFirestore.instance
+                .collection('users')
+                .doc(currentUserID)
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              }
+
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return CircularProgressIndicator();
+              }
+
+              final userDocument = snapshot.data;
+
+              if (!userDocument!.exists) {
+                return Text('No friend requests');
+              }
+
+              final friendRequests =
+                  userDocument['friendRequests'] as List<dynamic>;
+
+              if (friendRequests.isEmpty) {
+                return Text('No friend requests');
+              }
+
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: friendRequests.length,
+                itemBuilder: (context, index) {
+                  final request = friendRequests[index] as Map<String, dynamic>;
+                  final senderUsername = request['senderUsername'] as String?;
+
+                  // Check if senderUsername is null before using it
+                  if (senderUsername == null) {
+                    return Text('Error: Sender username not found');
+                  }
+                  return ListTile(
+                    title: Text(senderUsername),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            // Accept friend request
+                            // Implement the logic to accept the friend request
+                            // For example, you can update the user's friend list
+                            // and remove the request from the friendRequests list
+
+                            final friendRequest =
+                                friendRequests[index] as Map<String, dynamic>;
+                            final senderUsername =
+                                friendRequest['senderUsername'] as String?;
+                            final senderEmail =
+                                friendRequest['userID'] as String?;
+
+                            if (senderUsername != null && senderEmail != null) {
+                              // Add friend to the user's friend list
+                              FirebaseFirestore.instance
+                                  .collection('users')
+                                  .doc(currentUserID)
+                                  .update({
+                                'friendLists': FieldValue.arrayUnion([
+                                  {
+                                    'friendName': senderUsername,
+                                    'friendEmail': senderEmail
+                                  }
+                                ])
+                              });
+
+                              // Remove the friend request from the list
+                              FirebaseFirestore.instance
+                                  .collection('users')
+                                  .doc(currentUserID)
+                                  .update({
+                                'friendRequests':
+                                    FieldValue.arrayRemove([friendRequest])
+                              });
+                            }
+
+                            // After accepting, you may want to update the UI to reflect the change
+                          },
+                          child: Text('Accept'),
+                        ),
+                        SizedBox(width: 8),
+                        ElevatedButton(
+                          onPressed: () async {
+                            // Reject friend request
+                            // Implement the logic to reject the friend request
+                            // For example, you can remove the request from the friendRequests list
+
+                            final currentUserID =
+                                FirebaseAuth.instance.currentUser?.email;
+                            final senderUsername =
+                                request['senderUsername'] as String?;
+
+                            if (currentUserID != null &&
+                                senderUsername != null) {
+                              final userRef = FirebaseFirestore.instance
+                                  .collection('users')
+                                  .doc(currentUserID);
+
+                              await FirebaseFirestore.instance
+                                  .runTransaction((transaction) async {
+                                final userSnapshot =
+                                    await transaction.get(userRef);
+
+                                if (userSnapshot.exists) {
+                                  final friendRequests =
+                                      userSnapshot.data()?['friendRequests'] ??
+                                          [];
+
+                                  // Find the index of the friend request to remove
+                                  final index = friendRequests.indexWhere(
+                                      (request) =>
+                                          request['senderUsername'] ==
+                                          senderUsername);
+
+                                  if (index != -1) {
+                                    // Remove the friend request from the array
+                                    friendRequests.removeAt(index);
+
+                                    // Update the friendRequests field in Firestore
+                                    transaction.update(userRef,
+                                        {'friendRequests': friendRequests});
+                                  }
+                                }
+                              });
+                            }
+
+                            // After rejecting, you may want to update the UI to reflect the change
+                          },
+                          child: Text('Reject'),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
+          )
         ],
       ),
     );
