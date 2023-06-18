45c6a0644e2749a6e1478cc1478fa16877fe7255
Ashyraf <zmohdashyraf@gmail.com>
2023-06-19 01:37:10 +0800

    report notification

--- lib/screen/report.dart
+++ lib/screen/report.dart
@@ -1,5 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// ignore_for_file: unused_import
+
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:http/http.dart' as http;
 
 class ReportDialog extends StatefulWidget {
   final String roomName;
@@ -93,9 +98,76 @@ class _ReportDialogState extends State<ReportDialog> {
     }, SetOptions(merge: true)).then((value) {
       // Report sent successfully
       print('Report sent successfully!');
+      _sendNotificationToRoomMaster(widget.roomName);
     }).catchError((error) {
       // Error occurred while sending the report
       print('Error sending report: $error');
+    });
+  }
+
+  void _sendNotificationToRoomMaster(String roomName) {
+    FirebaseFirestore.instance
+        .collection('debtRoom')
+        .doc(roomName)
+        .get()
+        .then((roomSnapshot) {
+      if (roomSnapshot.exists) {
+        final roomMasterEmail = roomSnapshot.data()?['roomMaster'];
+        FirebaseFirestore.instance
+            .collection('users')
+            .where('email', isEqualTo: roomMasterEmail)
+            .get()
+            .then((userSnapshot) {
+          if (userSnapshot.docs.isNotEmpty) {
+            final roomMasterDoc = userSnapshot.docs.first;
+            final roomMasterToken = roomMasterDoc['fcmToken'];
+            final roomMasterUsername = roomMasterDoc['username'];
+
+            final serverKey =
+                'AAAA99JnmJg:APA91bGDu-pBEK8NsXEv91QzpnWjz9yajkEv9S_QTw578jmFSfFmCenNzh2Z0ggDtEFyLWcNfa1G4A9WKA8d34oOX7ctmlWF7pSerOFj40gBM6VgngcXwzyKj5jaiXIwFZYQn7NtmQ7Z';
+
+            final headers = <String, String>{
+              'Content-Type': 'application/json',
+              'Authorization': 'key=$serverKey',
+            };
+
+            final notification = {
+              'title': 'REPORT',
+              'body': 'There is a report from ${widget.friendName}.',
+            };
+
+            final message = {
+              'to': roomMasterToken,
+              'notification': notification,
+            };
+
+            http
+                .post(
+              Uri.parse('https://fcm.googleapis.com/fcm/send'),
+              headers: headers,
+              body: jsonEncode(message),
+            )
+                .then((response) {
+              print('FCM Response Code: ${response.statusCode}');
+              print('FCM Response Body: ${response.body}');
+              print('FCM Response Headers: ${response.headers}');
+
+              if (response.statusCode == 200) {
+                print('Notification sent successfully to $roomMasterUsername');
+              } else {
+                print(
+                    'Failed to send notification to $roomMasterUsername. Error: ${response.statusCode}');
+              }
+            }).catchError((error) {
+              print('Error sending notification: $error');
+            });
+          } else {
+            print('Room master not found.');
+          }
+        });
+      } else {
+        print('Debt room not found.');
+      }
     });
   }
 }
